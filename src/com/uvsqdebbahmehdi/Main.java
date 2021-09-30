8d30a0e18f4f5655e1ccddd4d1db9d191e2c132a
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-09-30 15:17:39 +0200

    ajout de to string de la classe fraction avec son constructeur  et methode de test main

--- src/com/uvsqdebbahmehdi/Main.java
+++ src/com/uvsqdebbahmehdi/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("ce ci cest une simple fraction ");
+        Fraction a = new Fraction(1,2);
+        System.out.println(a);
     }
 }
