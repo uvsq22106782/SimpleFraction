7e0d03ccda02f363ee8a957400a4ba95d72fc011
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-09-30 15:03:09 +0200

    ajout de la classe Main et la classe Fraction

---
+++ src/com/uvsqdebbahmehdi/Fraction.java
@@ -0,0 +1,4 @@
+package com.uvsqdebbahmehdi;
+
+public class Fraction {
+}
---
+++ src/com/uvsqdebbahmehdi/Main.java
@@ -0,0 +1,9 @@
+package com.uvsqdebbahmehdi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("ce ci cest une simple fraction ");
+    }
+}
